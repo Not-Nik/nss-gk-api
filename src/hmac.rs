6d41fad5837a1841c2ed5d008466b32ef42c73f0
Benjamin Beurdouche <beurdouche@mozilla.com>
2024-03-21 06:55:51 -0400

    Modify alg to mech mapping to be more platform independant

--- src/hmac.rs
+++ src/hmac.rs
@@ -9,17 +9,16 @@
 use crate::err::IntoResult;
 use crate::hash;
 use crate::hash::HashAlgorithm;
+use crate::p11;
 use crate::p11::PK11Origin;
 use crate::p11::PK11_CreateContextBySymKey;
 use crate::p11::PK11_DigestFinal;
 use crate::p11::PK11_DigestOp;
 use crate::p11::PK11_ImportSymKey;
 use crate::p11::Slot;
-// use crate::p11::SHA256_LENGTH;
 use crate::Error;
 use crate::SECItemBorrowed;
 use pkcs11_bindings::CKA_SIGN;
-// use pkcs11_bindings::CKM_SHA256_HMAC;
 use std::convert::TryFrom;
 use std::ptr;
 
@@ -33,11 +32,11 @@ pub enum HmacAlgorithm {
     HMAC_SHA2_512,
 }
 
-fn hmac_alg_to_ckm(alg: &HmacAlgorithm) -> Result<u64, Error> {
+fn hmac_alg_to_ckm(alg: &HmacAlgorithm) -> p11::CK_MECHANISM_TYPE {
     match alg {
-        HmacAlgorithm::HMAC_SHA2_256 => Ok(pkcs11_bindings::CKM_SHA256_HMAC),
-        HmacAlgorithm::HMAC_SHA2_384 => Ok(pkcs11_bindings::CKM_SHA384_HMAC),
-        HmacAlgorithm::HMAC_SHA2_512 => Ok(pkcs11_bindings::CKM_SHA512_HMAC),
+        HmacAlgorithm::HMAC_SHA2_256 => p11::CKM_SHA256_HMAC.into(),
+        HmacAlgorithm::HMAC_SHA2_384 => p11::CKM_SHA384_HMAC.into(),
+        HmacAlgorithm::HMAC_SHA2_512 => p11::CKM_SHA512_HMAC.into(),
     }
 }
 
@@ -66,7 +65,7 @@ pub fn hmac(alg: &HmacAlgorithm, key: &[u8], data: &[u8]) -> Result<Vec<u8>, Err
     let sym_key = unsafe {
         PK11_ImportSymKey(
             *slot,
-            hmac_alg_to_ckm(&alg)?,
+            hmac_alg_to_ckm(&alg),
             PK11Origin::PK11_OriginUnwrap,
             CKA_SIGN,
             SECItemBorrowed::wrap(key).as_mut(),
@@ -76,7 +75,7 @@ pub fn hmac(alg: &HmacAlgorithm, key: &[u8], data: &[u8]) -> Result<Vec<u8>, Err
     };
     let param = SECItemBorrowed::make_empty();
     let context = unsafe {
-        PK11_CreateContextBySymKey(hmac_alg_to_ckm(&alg)?, CKA_SIGN, *sym_key, param.as_ref())
+        PK11_CreateContextBySymKey(hmac_alg_to_ckm(&alg), CKA_SIGN, *sym_key, param.as_ref())
             .into_result()?
     };
     unsafe { PK11_DigestOp(*context, data.as_ptr(), data_len).into_result()? };
